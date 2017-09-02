76891048debd95e13e9db72a52731665b2cd6094
dhruvmalik95 <dhruvmalik@berkeley.edu>
2017-09-01 20:16:50 -0700

    s

--- rocksample-script.py
+++ rocksample-script.py
@@ -8,34 +8,43 @@ from rocksamplepomcp import *
 import math
 import pickle
 
-initial_state = (0,0)
-rock_vector = [((2,1),0), ((1,2),1)]
-theta_set = [[1,0],[0.3,0.8]]
-game = Rocksample_Game(3, 3, 1, 1, initial_state, rock_vector, theta_set, 0.95)
+big_l = []
+for _ in range(0, 20):
+	initial_state = (0,0)
+	rock_vector = [((2,1),0), ((1,2),1)]
+	theta_set = [[1,0],[0.3,0.8]]
+	game = Rocksample_Game(3, 3, 1, 1, initial_state, rock_vector, theta_set, 0.95)
 
-all_states = game.getAllStates()
-belief = []
+	all_states = game.getAllStates()
+	belief = []
 
-x = [initial_state, tuple(["no", "no"])]
-belief.append((x, 0))
-belief.append((x, 1))
-#belief.append((x, 2))
-print(belief)
+	x = [initial_state, tuple(["no", "no"])]
+	belief.append((x, 0))
+	belief.append((x, 1))
+	#belief.append((x, 2))
+	print(belief)
 
-initial_history = Root(game, belief, 0)
+	initial_history = Root(game, belief, 0)
 
-#make sure to change exploration accordingly - also what should the epsilon value be?
-epsilon = math.pow(0.95, 2)
+	#make sure to change exploration accordingly - also what should the epsilon value be?
+	epsilon = math.pow(0.95, 2)
 
-for _ in range(0, 1):
+
 #KEEP THESE PARAMETERS FOR NOW!!
-	solver = Rocksample_POMCP_Solver(0.95, epsilon, 1200000, initial_history, game, 500, 5)
+	solver = Rocksample_POMCP_Solver(0.95, epsilon, 600000, initial_history, game, 500, 5)
 	print(_)
 	solver.search()
 	data = solver.data
-	f = open('data-pomcp.txt', 'w')
-	f.write(str(data))
-	print("_____________________")
+	l = []
+	i = 3
+	while i <= 5.775:
+		l.append(data[round(10^i) - 1000])
+		i = i + 0.13875
+	big_l.append(l)	
+
+f = open('data-pomcp.txt', 'w')
+f.write(str(data))
+print("_____________________")
 
 """
 Things to keep in mind:
